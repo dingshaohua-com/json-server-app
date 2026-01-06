3f7037ee5b2ef5d5d059af800bbb565135ab64e7
丁少华 <960423114@qq.com>
2026-01-07 00:07:04 +0800

    Update server.js

--- api/server.js
+++ api/server.js
@@ -6,17 +6,17 @@ import jsonServer from 'json-server'
 const server = jsonServer.create()
 
 
-// // 使用默认中间件（日志、静态文件、CORS 和 no-cache）
-// server.use(jsonServer.defaults())
+// 使用默认中间件（日志、静态文件、CORS 和 no-cache）
+server.use(jsonServer.defaults())
 // 配置静态文件目录为 public
-server.use(jsonServer.defaults({ static: 'public' }))
+// server.use(jsonServer.defaults({ static: 'public' }))
 
 // 配置 URL 重写规则（需要在 router 之前使用）
 // '/api/*': '/$1' - 将 /api/posts 重写为 /posts
 // '/blog/:resource/:id/show': '/:resource/:id' - 将 /blog/posts/1/show 重写为 /posts/1
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1'
-}))
+// server.use(jsonServer.rewriter({
+//     '/api/*': '/$1'
+// }))
 
 
 // 使用路由中间件（路由中间件使用 db.json 作为数据源）
