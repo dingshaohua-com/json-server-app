b7d639b931136a1e7ee413fd33a48c5270ef3906
丁少华 <960423114@qq.com>
2026-01-07 00:18:25 +0800

    Update server.js

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ const server = jsonServer.create()
 // 使用默认中间件（日志、静态文件、CORS 和 no-cache）
 // server.use(jsonServer.defaults())
 // 配置静态文件目录为 public
-server.use(jsonServer.defaults({ static: 'public' }))
+server.use(jsonServer.defaults({ static: path.join(__dirname, '../public') }))
 
 // 配置 URL 重写规则（需要在 router 之前使用）
 // '/api/*': '/$1' - 将 /api/posts 重写为 /posts
